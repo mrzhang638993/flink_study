c6eec8f432ab6bf7272931167d0f216a51290bff
mrzhang638993 <1446719386@qq.com>
2022-05-04 15:41:50 +0800

    学习flink相关的代码实现

---
+++ src/main/java/spendreport/FraudDetector.java
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package spendreport;
+
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.walkthrough.common.entity.Alert;
+import org.apache.flink.walkthrough.common.entity.Transaction;
+
+/**
+ * Skeleton code for implementing a fraud detector.
+ */
+public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert> {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final double SMALL_AMOUNT = 1.00;
+	private static final double LARGE_AMOUNT = 500.00;
+	private static final long ONE_MINUTE = 60 * 1000;
+    private transient ValueState<Boolean> flagState;
+    //维护定时器的状态数据
+	private transient ValueState<Long> timerState;
+	@Override
+	public void processElement(
+			Transaction transaction,
+			Context context,
+			Collector<Alert> collector) throws Exception {
+		//最终对应的ValueState的数据记录,对应的表示的是上一次是否存储的是小数据,存储小数据的话,对应的是true,否则是空
+		Boolean value = flagState.value();
+		if (value!=null){
+			//先存储小数据，然后存储大数据
+			if (transaction.getAmount()>LARGE_AMOUNT){
+				//发出告警通知
+				Alert alert = new Alert();
+				alert.setId(transaction.getAccountId());
+				collector.collect(alert);
+			}
+			//上一次或者是这一次处理数据的话,对应的都是需要清理数据的。
+			/**
+			 * 上一次数据是小数据的话，这一次不论是否是大数据的话,对应的都是需要关闭的。
+			 * */
+			//flagState.clear();
+			cleanUp(context);
+		}
+		if (transaction.getAmount()<SMALL_AMOUNT){
+			//上一次存储的是小数据
+			flagState.update(true);
+			// set the timer and timer state
+			long timer = context.timerService().currentProcessingTime() + ONE_MINUTE;
+			context.timerService().registerProcessingTimeTimer(timer);
+			timerState.update(timer);
+		}
+	}
+
+	/**
+	 * 执行定时器的相关的任务实现操作
+	 * 超过了1分钟的话，清除对应的状态和定时器的数据。
+	 * */
+	@Override
+	public void onTimer(long timestamp, OnTimerContext ctx, Collector<Alert> out) throws Exception {
+		timerState.clear();
+		flagState.clear();
+	}
+
+	/**
+	 * 清除对应的定时器以及flagState的数据
+	 * */
+	private void cleanUp(Context ctx) throws Exception {
+		// delete timer
+		Long timer = timerState.value();
+		ctx.timerService().deleteProcessingTimeTimer(timer);
+		// clean up all state
+		timerState.clear();
+		flagState.clear();
+	}
+	/**
+	 * 注册ValueState,借助于ValueState实现容错处理实现
+	 * */
+	@Override
+	public void open(Configuration parameters) throws Exception {
+		ValueStateDescriptor<Boolean> valueStateDescriptor=new ValueStateDescriptor("flag",Boolean.class);
+		//注册flagState的状态描述符信息，ValueState包含了三个处理方法,update更新state状态值,value获取状态值,clear清理状态值。所有的操作必须执行对应的update更新操作
+		flagState= getRuntimeContext().getState(valueStateDescriptor);
+		ValueStateDescriptor<Long> longValueStateDescriptor=new ValueStateDescriptor<Long>("time",Long.class);
+		timerState=getRuntimeContext().getState(longValueStateDescriptor);
+		super.open(parameters);
+	}
+}
